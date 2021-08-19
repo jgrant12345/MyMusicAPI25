42ad45a7b93bf7995d2a5425917f2e56cc6accfe
Justin Grant <justin@Justins-Air.home>
2021-08-19 15:42:03 -0700

    change

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
-const app = express()
+var mysql = require('mysql');
 let port = process.env.PORT || 3000;
+require('dotenv').config()
+const app = express()
 
-app.get("/", (req,res) => {
-    res.send("Hello World")
+const host = process.env.MYHOST;
+const user = process.env.MYUSER;
+const password = process.env.MYPASSWORD;
+const database = process.env.MYDATABASE;
 
-})
+
+
+const db = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+});
+
+db.connect(function (err) {
+    if (err) throw err;
+    console.log('MySql Connected...');
+});
+
+app.get('/', (req, res) => {
+    const sql = 'SELECT * FROM Songs';
+
+        db.query(sql, (err, result) => {
+            if(err) throw err;
+            res.send(result);
+        });
+
+    
+});
 app.get("/2", (req,res) => {
     res.send("This is the other page!")
 })
