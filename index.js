3ba8e1caa3948cf4fe6cce0371592ac2a7321b9c
Justin Grant <justin@Justins-Air.home>
2021-08-19 13:36:05 -0700

    add

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get("/", (req,res) => {
     res.send("Hello World")
 
 })
+app.get("/2", (req,res) => {
+    res.send("This is the other page!")
+})
 app.listen(port,() =>{
     console.log(`app is listening on port ${port}`);
 })
